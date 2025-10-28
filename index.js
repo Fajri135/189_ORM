520e82869d6a54453c463045b09d71e252fb779b
Fajri135 <corex135@gmail.com>
2025-10-28 14:40:13 +0700

    Memastikan sinkronisasi database selesai sebelum server mulai mendengarkan koneksi

--- index.js
+++ index.js
@@ -10,3 +10,9 @@ app.listen(PORT, async () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
 
+db.sequelize.sync().then(() => {
+    app.listen(PORT, () => {
+        console.log(`server start`);
+    })
+})
+
